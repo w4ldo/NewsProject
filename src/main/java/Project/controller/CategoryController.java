d81b5c6b5c2f0fad382cae0f48cb284ef006d162
Matias Isosaari <matias_isosaari@hotmail.com>
2017-12-11 19:15:50 +0200

    functino to create categories added

--- src/main/java/Project/controller/CategoryController.java
+++ src/main/java/Project/controller/CategoryController.java
@@ -8,15 +8,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CategoryController {
-    
+
     @Autowired
     private NewsItemRepository newsItemRepository;
     @Autowired
     private CategoryRepository categoryRepository;
-    
+
     @GetMapping("/categories/{categoryId}")
     public String list(Model model, @PathVariable Long categoryId) {
         Category category = categoryRepository.getOne(categoryId);
@@ -24,5 +26,15 @@ public class CategoryController {
         model.addAttribute("categories", categoryRepository.findAll());
         return "newsItems";
     }
-    
+
+    @PostMapping("/categories/{newsItemId}")
+    public String create(@PathVariable Long newsItemId, @RequestParam String name) {
+
+        Category category = new Category();
+        category.setName(name);
+        categoryRepository.save(category);
+
+        return "redirect:/newsItems/{newsItemId}";
+    }
+
 }
