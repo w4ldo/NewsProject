64627c39ca2d9dee477ea6b5ccd8933b7d37070d
Matias Isosaari <matias_isosaari@hotmail.com>
2017-12-12 01:58:09 +0200

    fixing heroku3

--- src/main/java/Project/config/ProductionProfile.java
+++ src/main/java/Project/config/ProductionProfile.java
@@ -1,26 +1,20 @@
 package Project.config;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import javax.activation.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 
-@Configuration
 @Profile("production")
+@Configuration
 public class ProductionProfile {
 
     @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-        return basicDataSource;
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        System.out.println("Hello world!");
+        return DataSourceBuilder.create().build();
     }
 }
