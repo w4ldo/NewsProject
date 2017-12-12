c63772cbf34180ac9374e4dd60750540b1c895ed
Matias Isosaari <matias_isosaari@hotmail.com>
2017-12-12 02:10:16 +0200

    fixing heroku5

--- src/main/java/Project/config/ProductionProfile.java
+++ src/main/java/Project/config/ProductionProfile.java
@@ -1,11 +1,14 @@
 package Project.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 @Profile("production")
 @Configuration
